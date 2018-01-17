2b2dfcad9f20858fd437f48428fc545a4382fde0
Dave Harlan <dkharlan@umich.edu>
2018-01-17 10:04:03 -0500

    Add a button and method to the frontend for POSTing

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,86 +1,42 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}<span v-if="name">, {{ name }}!</span></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}<span v-if="name">, {{ name }}!</span></h1>
+        <button v-on:click="sendMessage">Send A Message</button>
+        <h2>Essential Links</h2>
+        <ul>
+            <li>
+                <a href="https://vuejs.org" target="_blank">Core Docs</a>
+            </li>
+            <li>
+                <a href="https://forum.vuejs.org" target="_blank">Forum</a>
+            </li>
+            <li>
+                <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
+            </li>
+            <li>
+                <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+            </li>
+            <br>
+            <li>
+                <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
+            </li>
+        </ul>
+        <h2>Ecosystem</h2>
+        <ul>
+            <li>
+                <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+            </li>
+            <li>
+                <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+            </li>
+            <li>
+                <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
+            </li>
+            <li>
+                <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -98,6 +54,15 @@ export default {
       msg: 'Welcome to Your Vue.js App',
       name: null
     };
+  },
+  methods: {
+    sendMessage() {
+      fetch(__API_URL__ + '/tell_me_something/', {
+        credentials: 'include',
+        method: 'post',
+        body: JSON.stringify({message: 'Hello from the frontend!'})
+      });
+    }
   }
 };
 </script>
