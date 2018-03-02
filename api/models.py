0339ba66b8129385f5b9c5a512d2652ed93ab3d7
Dave Harlan <david.k.harlan@gmail.com>
2018-03-02 18:21:55 -0500

    Add TodoItem model and migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.db.models import Model, TextField
 
-# Create your models here.
+
+class TodoItem(Model):
+    text = TextField()
+
+    def to_dict(self):
+        return {'id': self.id, 'text': self.text}
