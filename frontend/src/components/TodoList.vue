5918a909f26e5534cd6b2af996ca7507791a2991
Dave Harlan <david.k.harlan@gmail.com>
2018-03-03 20:44:08 -0500

    Add simple frontend

---
+++ frontend/src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <todo-item v-for="item of items" :text="item.text"/>
+    </ul>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem';
+
+export default {
+  name: "todo-list",
+  components: {TodoItem}
+};
+</script>
+
+<style scoped>
+
+</style>
